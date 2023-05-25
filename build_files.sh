3a40e13a89cbfb366d97dbf12349b1e808f73fdc
Lolade <Shitturidwan@gmail.com>
2023-05-25 10:59:29 +0100

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,11 @@
 # build_files.sh
 pip install -r requirements.txt
-#python3.11.0 manage.py collectstatic
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Build static assets
+python manage.py collectstatic --noinput
+
+# Start the Django development server
+python manage.py runserver 0.0.0.0:$PORT
