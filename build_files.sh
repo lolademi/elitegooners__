6ea5481cb94f24469d72ee7accbedc6ad2da0cb1
Lolade <39669395+lolademi@users.noreply.github.com>
2023-05-25 21:25:43 +0100

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,15 +1,13 @@
-# build_files.sh
-
 #!/bin/bash
 
 # Install dependencies
 pip install -r requirements.txt
 
 # Build static assets
-python3.11.0 manage.py collectstatic --noinput
+python3.9 manage.py collectstatic --noinput
 
 # Run any necessary database migrations
-python3.11.0 manage.py migrate
+python3.9 manage.py migrate
 
 # Start the Django development server
-python3.11.0 manage.py runserver 0.0.0.0:$PORT
+python3.9 manage.py runserver 0.0.0.0:$PORT
